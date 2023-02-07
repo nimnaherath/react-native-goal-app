6a91030d0018b48aee53eea342790391221ce62f
Nimna <avantha.nimna@gmail.com>
2023-02-07 13:16:21 +0530

    Add sum styles

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,7 +2,11 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this,props.id )}>
+    <Pressable
+      android_ripple={{ color: "#dddddd" }}
+      onPress={props.onDeleteItem.bind(this, props.id)}
+      style={({ preesd }) => preesd && styles.preesedItem}
+    >
       <View style={styles.goalItem}>
         <Text style={styles.goalText}>{props.text}</Text>
       </View>
@@ -15,11 +19,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
   goalText: {
     color: "white",
+    padding: 8,
+  },
+  preesedItem: {
+    opacity: 0.5,
   },
 });
